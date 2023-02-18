6a3d234dca8fe80771cb76c85f470732891c9d1a
siwakorntumnao <125736600+siwakorntumnao@users.noreply.github.com>
2023-02-18 18:25:36 +0700

    first homework

---
+++ homework 1.py
@@ -0,0 +1,41 @@
+import turtle
+
+tao=turtle.Pen
+tao=turtle.Pen()
+tao.shape('turtle')
+
+tao.clear()
+for i in range (4):
+    tao.forward(200)
+    tao.left(90)
+
+
+tao.penup()
+tao.goto(300,0)
+tao.pendown()
+
+for i in range (4):
+    tao.forward(200)
+    tao.left(90)
+
+
+    
+
+tao.penup()
+tao.goto(300,-300)
+tao.pendown()
+
+for i in range (4):
+    tao.forward(200)
+    tao.left(90)
+
+tao.penup()
+tao.goto(0,-300)
+tao.pendown()
+
+for i in range (4):
+    tao.forward(200)
+    tao.left(90)
+
+    
+
